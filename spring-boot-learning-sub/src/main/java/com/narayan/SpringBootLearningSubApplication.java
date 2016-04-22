34bf541e7be9f92ad28b221b83006f02b646c93d
unmattavesa <unmattavesa@gmail.com>
2016-04-22 15:21:45 +0530

    Create SpringBootLearningSubApplication.java

---
+++ spring-boot-learning-sub/src/main/java/com/narayan/SpringBootLearningSubApplication.java
@@ -0,0 +1,12 @@
+package com.narayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public abstract class SpringBootLearningSubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningSubApplication.class, args);
+	}
+}
