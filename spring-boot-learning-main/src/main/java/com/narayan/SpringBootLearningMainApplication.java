68dbbc13af088ee4356b1e8cc071e60a80fda556
unmattavesa <unmattavesa@gmail.com>
2016-04-22 15:16:59 +0530

    Create SpringBootLearningMainApplication.java

---
+++ spring-boot-learning-main/src/main/java/com/narayan/SpringBootLearningMainApplication.java
@@ -0,0 +1,12 @@
+package com.narayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningMainApplication.class, args);
+	}
+}
